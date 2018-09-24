17463608ea7a191edafa804b21842aba2c1341ac
Charles Clemens <cclemensjr@gmail.com>
2018-09-24 13:20:07 -0700

    Created user stories

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
 'use strict';
+
+// Display potential products in a row of 3;
+    //May need to manage size size of and/or edit images
+
+// The user needs to click on 1 of the three images
+
+// The image selected and the number of clicks needs to be stored
+
+//Show the user has selected 25 images (clicked 25 times) they are shown a graph with the images and the number of clicks per image
+
+//The percentage of times an image has been shown needs to be stored
+    //Store times shown in a variable then calculate percentage
