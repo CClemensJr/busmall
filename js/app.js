3a646bd31fe1fd9a52645c1a855a7a2d96cbd996
Charles Clemens <cclemensjr@gmail.com>
2018-09-24 17:49:45 -0700

    Implementing click counting of Products

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 
 // DONE - Store the clicks in a global variable 
 var totalClicks = 0;
-var images = [['/img/bag.jpg', '/img/banana.jpg', '/img/bathroom.jpg', '/img/boots.jpg', '/img/breakfast.jpg',      'img/bubblegum.jpg', '/img/chair.jpg', '/img/cthulu.jpg', '/img/dog-duck.jpg', '/img/dragon.jpg', '/img/pen.jpg','/img/pet-sweep.jpg', '/img/scissors.jpg','/img/shark.jpg','/img/sweep.png','/img/tauntaun.jpg','/img/unicorn.jpg','/img/usb.gif','/img/water-can.jpg','/img/wine-glass.jpg'], ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass']];
+var images = [['/img/bag.jpg', '/img/banana.jpg', '/img/bathroom.jpg', '/img/boots.jpg', '/img/breakfast.jpg',      'img/bubblegum.jpg', '/img/chair.jpg', '/img/cthulhu.jpg', '/img/dog-duck.jpg', '/img/dragon.jpg', '/img/pen.jpg','/img/pet-sweep.jpg', '/img/scissors.jpg','/img/shark.jpg','/img/sweep.png','/img/tauntaun.jpg','/img/unicorn.jpg','/img/usb.gif','/img/water-can.jpg','/img/wine-glass.jpg'], ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass']];
 
 var productOnLeft = document.getElementById('product1');
 var productInMiddle = document.getElementById('product2');
@@ -22,49 +22,95 @@ function Product(filepath, filename) {
 //DONE - Push each instance into an object array
 Product.allProducts = [];
 
-//DONE - Create instances of the Product object
-for (var i = 0; i < images[0].length; i++)
+Product.prototype.countClicked = function()
 {
-  new Product(images[0][i], images[1][i]);
-}
+    this.productSelected += 1;
+};
+
+Product.prototype.countShown = function()
+{
+    this.productShown += 1;
+}; 
 
 
-  //Use a function to put 3 random images onto the page
-function showProducts()
+//DONE - Use a function to put 3 random images onto the page
+//DONE - Make sure that each item is different.
+//DONE - Add 1 to the item shown counter for each item object shown
+function showProducts(event)
 {
     var leftNum = rando(0, Product.allProducts.length);
     var midNum = rando(0, Product.allProducts.length);
     var rightNum = rando(0, Product.allProducts.length);
 
-    productOnLeft.src = Product.allProducts[leftNum].productImgSrc;
-    productOnLeft.alt = Product.allProducts[leftNum].productName;
+    if (event)
+    {
+        totalClicks += 1;
 
-    productInMiddle.src = Product.allProducts[midNum].productImgSrc;
-    productInMiddle.alt = Product.allProducts[midNum].productName;
+        for (var i = 0; i < Product.allProducts.length; i++)
+        {
+             if (event.target.alt === Product.allProducts[i].productName)
+             {
+                 Product.allProducts[i].countClicked();
+                 break;
+             }
+        }
 
-    productOnRight.src = Product.allProducts[rightNum].productImgSrc;
-    productOnRight.alt = Product.allProducts[rightNum].productName;
-};
+    }
+    
+    if (totalClicks === 25)
+    {
+        console.log('25 CLICKS!');
+    }
+
+    if (leftNum !== midNum && leftNum !== rightNum && midNum !== rightNum )
+    {
+        productOnLeft.src = Product.allProducts[leftNum].productImgSrc;
+        productOnLeft.alt = Product.allProducts[leftNum].productName;
+
+        productInMiddle.src = Product.allProducts[midNum].productImgSrc;
+        productInMiddle.alt = Product.allProducts[midNum].productName;
+
+        productOnRight.src = Product.allProducts[rightNum].productImgSrc;
+        productOnRight.alt = Product.allProducts[rightNum].productName;
+        
+        Product.allProducts[leftNum].countShown();
+        Product.allProducts[midNum].countShown();
+        Product.allProducts[rightNum].countShown();
+    }
+    else
+    {
+        showProducts();
+    }
+
+    console.log(Product.allProducts);
+}
 
 
 function rando(min, max)
 {
   var randomNumber = Math.floor(Math.random() * (max - min)) + min;
- 
+
   return randomNumber;
 }
-
-showProducts();
-  //Make sure that each item is different.
-  //Add 1 to the item shown counter for each item object shown
   // Clear the images then show 3 new items
 
 
 // The user needs to click on 1 of the three images
-  // Create an event listener for each img tag
+  // DONE - Create an event listener for each img tag
+  productOnLeft.addEventListener('click', showProducts);
+  productInMiddle.addEventListener('click', showProducts);
+  productOnRight.addEventListener('click', showProducts);
+
   // Call a function that collects the click and the image selected
   // Store the clicks in a global variable and image selected in an instance property.
 
+  //DONE - Create instances of the Product object
+for (var i = 0; i < images[0].length; i++)
+{
+  new Product(images[0][i], images[1][i]);
+}
+
+showProducts();
 //Show the user has selected 25 images (clicked 25 times) they are shown a graph with the images and the number of clicks per image
   // Turn off the event listeners
   // Display a list of items with the number of votes received ie "3 votes for Banana Slicer"
