fa9a73d9c03de8cf9451a4ddd991879fa354eb98
Charles Clemens <cclemensjr@gmail.com>
2018-09-30 23:48:31 -0700

    Fixed bugs

--- js/app.js
+++ js/app.js
@@ -9,18 +9,20 @@ var productOnLeft = document.getElementById('product1');
 var productInMiddle = document.getElementById('product2');
 var productOnRight = document.getElementById('product3');
 var productElements = [productOnLeft, productInMiddle, productOnRight];
-var randoArray = [];
+var currentProducts = [];
+var previousProducts = [];
 
 
-function Product(filepath, filename, productShown = 0, productClicked = 0) {
+function Product(filepath, filename) {
   this.productImgSrc = filepath;
   this.productName = filename;
-  this.productShown = productShown;
-  this.productClicked = productClicked;
+  this.productShown = 0;
+  this.productClicked = 0;
 
   Product.allProducts.push(this);
 }
 Product.allProducts = [];
+
 
 function loadProducts() {
   var localProducts = localStorage.getItem('products');
@@ -40,213 +42,155 @@ function loadProducts() {
   console.log(Product.allProducts);
 }
 
+function showProducts()
+{
+  var randomProducts = getRandomProducts();
+  console.log(`In show products. received ${randomProducts} from getRandomProducts()`);
+
+  for (var j = 0; j < randomProducts.length; j++)
+  {
+    productElements[j].src = Product.allProducts[randomProducts[j]].productImgSrc;
+    productElements[j].alt = Product.allProducts[randomProducts[j]].productName;
+
+    Product.allProducts[randomProducts[j]].productShown++;
+  }
+  console.log(Product.allProducts);
+}
+
+function getRandomProducts()
+{
+  currentProducts = [];
+
+  for (var i = 0; i < productElements.length; i++)
+  {
+    var shown = false;
+
+    while (!shown) {
+      var n = rando(0, Product.allProducts.length - 1);
+
+      if (!previousProducts.includes(n))
+      {
+        if (!currentProducts.includes(n))
+        {
+          currentProducts.push(n);
+          shown = true;
+        }
+      }
+    }
+  }
+
+  previousProducts = currentProducts;
+
+  return currentProducts;
+}
+
+function rando(min, max)
+{
+  var randomNumber = Math.floor(Math.random() * (max - min)) + min;
+
+  return randomNumber;
+}
+
+function getSelectionRate(clicked, shown)
+{
+  var results = Math.floor((clicked / shown) * 100);
+  if (results)
+  {
+    return results;
+  }
+  else
+  {
+    return 0;
+  }
+}
+
+function createChart()
+{
+  var timesShown = [];
+  var timesSelected = [];
+  var selectionRates = [];
+
+  for (var i = 0; i < Product.allProducts.length; i++)
+  {
+    timesShown.push(Product.allProducts[i].productShown);
+    timesSelected.push(Product.allProducts[i].productClicked);
+    selectionRates.push(getSelectionRate(Product.allProducts[i].productClicked, Product.allProducts[i].productShown));
+  }
+
+  var ctx = document.getElementById('myChart').getContext('2d');
+  var shownOrSelectedChart = new Chart(ctx, {
+    type: 'horizontalBar',
+    data: {
+      labels: images[1],
+      datasets: [{
+        label: 'Times Shown',
+        data: timesShown,
+        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+        borderColor: 'rgba(255,99,132,1)',
+        borderWidth: 1
+      },
+      {
+        label: 'Times Selected',
+        data: timesSelected,
+        backgroundColor: 'rgba(54, 162, 235, 0.2)',
+        borderColor: 'rgba(54, 162, 235, 1)',
+        borderWidth: 2
+      },
+      {
+        label: 'Selection Percentage',
+        data: selectionRates,
+        backgroundColor: 'rgba(255, 206, 86, 0.2)',
+        borderColor: 'rgba(255, 206, 86, 1)',
+        borderWidth: 3
+      }],
+    },
+    options: {
+      scales: {
+        xAxes: [{
+          ticks: {
+            max: 100,
+            min: 0,
+            stepSize: 10
+          }
+        }],
+        yAxes: [{
+        }]
+      }
+    }
+  });
+}
+
+function handleClick(event){
+  for (var i = 0; i < Product.allProducts.length; i++)
+  {
+    if (event.target.alt === Product.allProducts[i].productName)
+    {
+      Product.allProducts[i].productClicked++;
+    }
+  }
+
+  totalClicks++;
+
+  if (totalClicks >= maxClicks){
+    toggleEventHandler();
+    localStorage.setItem('products', JSON.stringify(Product.allProducts));
+    createChart();
+  }
+  else {
+    showProducts();
+  }
+}
+
+function toggleEventHandler()
+{
+  if (totalClicks >= maxClicks){
+    productImages.removeEventListener('click', handleClick);
+  }
+  else {
+    productImages.addEventListener('click', handleClick);
+  }
+}
+
 loadProducts();
-// Product.prototype.countClicked = function()
-// {
-//   this.productSelected++;
-//   //localStorage.setItem(this.productName, JSON.stringify(this));
-// };
-
-// Product.prototype.countShown = function()
-// {
-//   this.productShown++;
-//   //localStorage.setItem(this.productName, JSON.stringify(this));
-// };
-
-// Product.prototype.selectionRate = function()
-// {
-//   var results = Math.floor(((this.productSelected / totalClicks) * 100));
-
-//   if (NaN)
-//   {
-//     return 0;
-//   }
-//   else
-//   {
-//     return results;
-//   }
-// };
-
-
-// function createProducts()
-// {
-//   for (var i = 0; i < images[0].length; i++)
-//   {
-//     new Product(images[0][i], images[1][i]);
-//   }
-// }
-
-
-// function showProducts()
-// {
-//   var randomProducts = [rando(0, Product.allProducts.length), rando(0, Product.allProducts.length), rando(0, Product.allProducts.length)];
-
-//   if (checkUniqueness(randomProducts) === true && checkLastShown(randomProducts) === false)
-//   {
-//     for (var j = 0; j < randomProducts.length; j++)
-//     {
-//       productElements[j].src = Product.allProducts[randomProducts[j]].productImgSrc;
-//       productElements[j].alt = Product.allProducts[randomProducts[j]].productName;
-
-//       Product.allProducts[randomProducts[j]].countShown();
-//     }
-//   }
-//   else
-//   {
-//     showProducts();
-//   }
-//   console.log(Product.allProducts);
-// }
-
-// function rando(min, max)
-// {
-//   var randomNumber = Math.floor(Math.random() * (max - min)) + min;
-
-//   return randomNumber;
-// }
-
-// function checkTotalClicks()
-// {
-//   totalClicks += 1;
-//   console.log('Total Clicks = ', totalClicks);
-//   if (totalClicks === 25)
-//   {
-//     localStorage.setItem("products", JSON.stringify(Product.allProducts));
-//     productImages.removeEventListener('click', eventHandler);
-//     createChart();
-//   }
-// }
-
-// function checkUniqueness(array)
-// {
-//   //console.log('In checkUniqueness');
-//   var counts = [];
-
-//   for (var i = 0; i < array.length; i++)
-//   {
-//     ///console.log(`The number ${i} is ${array[i]}`);
-//     if (counts[array[i]] === undefined)
-//     {
-//       counts[array[i]] = 1;
-//     }
-//     else
-//     {
-//       ///console.log('The numbers are not unique');
-//       return false;
-//     }
-//   }
-//   //console.log('The numbers are unique');
-//   return true;
-// }
-
-// function checkLastShown(array)
-// {
-//   var notSame = true;
-
-//   if (randoArray[0] === undefined)
-//   {
-//     randoArray = array;
-//   }
-//   else
-//   {
-//     for (var i = 0; i < array.length; i++)
-//     {
-//       if (randoArray.includes(array[i]))
-//       {
-//         notSame = false;
-
-//         return true;
-//       }
-//     }
-
-//     if (notSame === true)
-//     {
-//       randoArray = array;
-//       return false;
-//     }
-//   }
-// }
-
-// function setEventListeners()
-// {
-//   productImages.addEventListener('click', eventHandler);
-// }
-
-
-// function eventHandler(event){
-//   //event.stopPropagation();
-
-//   for (var i = 0; i < Product.allProducts.length; i++)
-//   {
-//     if (event.target.alt === Product.allProducts[i].productName)
-//     {
-
-//       Product.allProducts[i].countClicked();
-
-//       break;
-//     }
-//   }
-
-//   checkTotalClicks();
-//   showProducts();
-// }
-
-// function createChart()
-// {
-//   var timesShown = [];
-//   var timesSelected = [];
-//   var selectionRates = [];
-
-//   for (var i = 0; i < Product.allProducts.length; i++)
-//   {
-//     timesShown.push(Product.allProducts[i].productShown);
-//     timesSelected.push(Product.allProducts[i].productSelected);
-//     selectionRates.push(Product.allProducts[i].selectionRate());
-//   }
-
-//   var ctx = document.getElementById('myChart').getContext('2d');
-//   var shownOrSelectedChart = new Chart(ctx, {
-//     type: 'horizontalBar',
-//     data: {
-//       labels: images[1],
-//       datasets: [{
-//         label: 'Times Shown',
-//         data: timesShown,
-//         backgroundColor: 'rgba(255, 99, 132, 0.2)',
-//         borderColor: 'rgba(255,99,132,1)',
-//         borderWidth: 1
-//       },
-//       {
-//         label: 'Times Selected',
-//         data: timesSelected,
-//         backgroundColor: 'rgba(54, 162, 235, 0.2)',
-//         borderColor: 'rgba(54, 162, 235, 1)',
-//         borderWidth: 2
-//       },
-//       {
-//         label: 'Selection Percentage',
-//         data: selectionRates,
-//         backgroundColor: 'rgba(255, 206, 86, 0.2)',
-//         borderColor: 'rgba(255, 206, 86, 1)',
-//         borderWidth: 3
-//       }],
-//     },
-//     options: {
-//       scales: {
-//         xAxes: [{
-//           ticks: {
-//             max: totalClicks / 2,
-//             min: 0,
-//             stepSize: 1
-//           }
-//         }],
-//         yAxes: [{
-//         }]
-//       }
-//     }
-//   });
-// }
-
-// createProducts();
-// setEventListeners();
-// showProducts();
+showProducts();
+toggleEventHandler();
